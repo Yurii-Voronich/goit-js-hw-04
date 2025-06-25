79d3e43b39c732013cf17c0410ec78509cf43c85
YURII VORONYCH <voronich97@gmail.com>
2025-06-25 16:15:00 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(array, value) {
+  let newArrey = [];
+  for (const numbers of array) {
+    if (numbers > value) {
+      newArrey.push(numbers);
+    }
+  }
+  return newArrey;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
